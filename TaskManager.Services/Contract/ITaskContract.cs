d953acee618a8c8f595b4636bb0bbbc825cdf1c2
Lipindim <lipindim@gmail.com>
2017-06-12 17:27:46 +1000

    Добавил контракт для задач

---
+++ TaskManager.Services/Contract/ITaskContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Services.Contract
+{
+    [ServiceContract]
+    public interface ITaskContract
+    {
+        Task GetTask(int taskID);
+        List<Task> GetTasks(int userId);
+        void AddTask(Task newTask);
+        void CancelTask(int taskID);
+        void ChangePercentComplete(int taskID, int percent);
+        void AddComment(string text, int taskID, int userID);
+        List<Comment> GetComments(int taskID);
+    }
+}
---
+++ TaskManager.Services/Contract/TaskContract.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Services.Contract
+{
+    public class TaskContract : ITaskContract
+    {
+
+        private Repository<Task> taskRepository;
+        public Repository<Task> TaskRepository
+        {
+            get
+            {
+                if (taskRepository == null)
+                {
+                    taskRepository = new Repository<Task>(GlobalSettings.Context);
+                }
+                return taskRepository;
+            }
+
+            set
+            {
+                taskRepository = value;
+            }
+        }
+
+        private Repository<Alert> alertRepository;
+        public Repository<Alert> AlertRepository
+        {
+            get
+            {
+                if (alertRepository == null)
+                {
+                    alertRepository = new Repository<Alert>(GlobalSettings.Context);
+                }
+                return alertRepository;
+            }
+
+            set
+            {
+                alertRepository = value;
+            }
+        }
+
+        private Repository<Comment> commentRepository;
+        public Repository<Comment> CommentRepository
+        {
+            get
+            {
+                if (commentRepository == null)
+                {
+                    commentRepository = new Repository<Comment>(GlobalSettings.Context);
+                }
+                return commentRepository;
+            }
+
+            set
+            {
+                commentRepository = value;
+            }
+        }
+
+        public void AddComment(string text, int taskID, int userID)
+        {
+            commentRepository.AddItem(new Comment()
+            {
+                TaskID = taskID,
+                Text = text,
+                UserID = userID,
+                TimeCreated = DateTime.Now
+            });
+        }
+
+        public void AddTask(Task newTask)
+        {
+            TaskRepository.AddItem(newTask);
+        }
+
+        public void CancelTask(int taskID)
+        {
+            Task task = TaskRepository.GetItem(x => x.ID == taskID);
+            task.CurrentStatus = Status.Canceled;
+            taskRepository.SaveChanges();
+
+            CreateAlert(AlertType.Canceled, taskID, task.ExecutorID, task.ManagerID);
+        }
+
+        public void ChangePercentComplete(int taskID, int percent)
+        {
+            Task task = TaskRepository.GetItem(x => x.ID == taskID);
+            task.PersentComplete = percent;
+            TaskRepository.SaveChanges();
+
+            if (percent >= 100)
+            {
+                CreateAlert(AlertType.Executed, taskID, task.CreatorID, task.ManagerID);
+            }
+        }
+
+        public List<Comment> GetComments(int taskID)
+        {
+            return CommentRepository.GetItems(x => x.TaskID == taskID);
+        }
+
+        public Task GetTask(int taskID)
+        {
+            return TaskRepository.GetItem(x => x.ID == taskID);
+        }
+
+        public List<Task> GetTasks(int userId)
+        {
+            return TaskRepository.GetItems(x => x.ManagerID == userId || x.ExecutorID == userId || x.CreatorID == userId);
+        }
+
+        public void CreateAlert(AlertType alertType, int taskID, params int[] usersID)
+        {
+            foreach (var userID in usersID.Distinct())
+            {
+                AlertRepository.AddItem(new Alert()
+                {
+                    AlertType = AlertType.Executed,
+                    TaskID = taskID,
+                    UserID = userID
+                });
+            } 
+        }
+    }
+}
